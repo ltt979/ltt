c41720c9fe8b8fd748625a9c4a64b1d5bbbf6418
ltt979 <947671554@qq.com>
2018-04-23 21:25:46 +0800

    init

---
+++ app.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var settings = require('./settings');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// app.set("view engine","ejs");
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// app.get("/",router.showIndex);
+
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
+
+app.use(session({
+  secret: settings.cookieSecret,
+  key: settings.db,//cookie name
+  cookie: {maxAge: 1000 * 60 * 60 * 24 * 30},//30 days
+  store: new MongoStore({
+    url: 'mongodb://localhost[:27017]/blog',
+    db: settings.db,
+    host: settings.host,
+    port: settings.port
+  })
+}));
+
+module.exports = app;
+
+
