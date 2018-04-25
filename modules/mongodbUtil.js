319bfebd213ff565441c164b949bb5aef69c77b7
U-EHI\michonnel <Michonne.Liao@ehealth.com>
2018-04-25 23:32:59 +0800

    mongodb bug fix

--- modules/mongodbUtil.js
+++ modules/mongodbUtil.js
@@ -2,20 +2,24 @@ const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
 
 // Connection URL
-const url = 'mongodb://localhost:27017';
+const url = 'mongodb://127.0.0.1:27017';
 
 // Database Name
 const dbName = 'blog';
 var mongoblog;
 
 // Use connect method to connect to the server
-MongoClient.connect(url, function (err, client) {
+MongoClient.connect(url, {
+    reconnectTries: 30
+    , keepAlive: 1
+}, function (err, client) {
     assert.equal(null, err);
     console.log("Connected successfully to server");
 
     const db = client.db(dbName);
     mongoblog = db;
-    client.close();
+    // keep alive 
+    // client.close();
 });
 
 
